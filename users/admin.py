f4f09baf179d8653370467cec477a57fe8ba33d7
mrdjangodev <mirolimcoder@gmail.com>
2022-10-23 21:27:14 +0300

    User model created and django jazzmin connected

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,22 @@
+from itertools import count
+import stat
 from django.contrib import admin
+from .models import User
 
 # Register your models here.
+
+
+
+class UserAdmin(admin.ModelAdmin):
+
+    # def count_followers(self, obj):
+    #     return obj.followers.count()
+
+    # def count_followings(self, obj):
+    #     return obj.followings.count()
+    
+    list_display = ['username', 'email']
+    # count_followers.short_description = 'Count followers'
+    # count_followers.admin_order_field = 'user.count_followers'
+
+admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,37 @@
+from random import choices
+from secrets import choice
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    COUNTRY_CHOICES = (
+        ('Uzbekistan', 'Uzbekistan'),
+        ("USA", "USA"),
+        ("Russia", "Russia"),
+        ('Turkey', 'Turkey'),
+        ('Other', 'Other'),
+    )
+
+    GENDER_CHOICES = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
+
+    AGE_CHOICES = (
+        ('14-17', '14-17'),
+        ('18-27', '18-27'),
+        ('28-45', '28-45'),
+        ('46-60', '46-60'),
+        ('61-', '61-'),
+    )
+    alternative_email = models.EmailField(blank=True, null=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    age = models.CharField(max_length=5, choices=AGE_CHOICES, blank=True, null=True)
+    country = models.CharField(max_length=25, choices=COUNTRY_CHOICES, blank=True, null=True)
+    followers = models.ManyToManyField('self', blank=True)
+    followings = models.ManyToManyField('self', blank=True)
+
+    def __str__(self) -> str:
+        return super().username
